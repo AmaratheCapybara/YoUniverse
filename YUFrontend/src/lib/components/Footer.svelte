652a9d7bc49a822cb94c07fbdaca0e600ba3325c
AmaratheCapybara <AmaranthAragon@gmail.com>
2025-04-16 18:50:16 -0400

    button for testing footer
    changed one of the footer buttons to reflect a custom class in tailwind. Haven't gotten it to work yet
    
    Co-Authored-By: Jaenatheseastar <184808812+Jaenatheseastar@users.noreply.github.com>

--- YUFrontend/src/lib/components/Footer.svelte
+++ YUFrontend/src/lib/components/Footer.svelte
@@ -1,10 +1,11 @@
 <script>
     import {ButtonGroup, Button, GradientButton, Footer} from 'svelte-5-ui-lib';
+    import '../../app.css';
 </script>
 <Footer footerType="sticky" class="absolute flex justify-stretch justify-center bg-transparent">
 
-    <ButtonGroup id="FooterButtons" class=" flex justify-stretch justify-center bg-[#77B602]">
-        <Button href="https://ko-fi.com/youniverseproject" class="bg-[#77B602]">Donation</Button>
+    <ButtonGroup id="FooterButtons" class=" flex justify-stretch justify-center bg-[#77B602] center">
+        <Button href="https://ko-fi.com/youniverseproject" class="basicBitchButton">Donation</Button>
         <Button href="https://www.redbubble.com/people/TheYoUniverse/shop?asc=u"  class="bg-[#77B602]">Merch</Button>
         <Button href="../about"  class="bg-[#77B602]">About</Button>
     </ButtonGroup>
