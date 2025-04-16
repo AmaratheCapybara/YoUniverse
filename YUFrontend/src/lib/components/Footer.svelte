141a6e0c303ceaf8b6e53fad8f6dac4de032723e
AmaratheCapybara <AmaranthAragon@gmail.com>
2025-04-16 16:36:06 -0400

    updated footer buttons
    added flex justify-stretch
    made footer transparent

--- YUFrontend/src/lib/components/Footer.svelte
+++ YUFrontend/src/lib/components/Footer.svelte
@@ -1,9 +1,9 @@
 <script>
     import {ButtonGroup, Button, GradientButton, Footer} from 'svelte-5-ui-lib';
 </script>
-<Footer footerType="sticky" class="absolute flex justify-stretch justify-center">
+<Footer footerType="sticky" class="absolute flex justify-stretch justify-center bg-transparent">
 
-    <ButtonGroup id="FooterButtons" class="justify-center bg-[#77B602]">
+    <ButtonGroup id="FooterButtons" class=" flex justify-stretch justify-center bg-[#77B602]">
         <Button href="https://ko-fi.com/youniverseproject" class="bg-[#77B602]">Donation</Button>
         <Button href="https://www.redbubble.com/people/TheYoUniverse/shop?asc=u"  class="bg-[#77B602]">Merch</Button>
         <Button href="../about"  class="bg-[#77B602]">About</Button>
