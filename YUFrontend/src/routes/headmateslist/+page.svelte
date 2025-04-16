d69de1030130d7dd3656cc2106a08407d61ec7b3
AmaratheCapybara <AmaranthAragon@gmail.com>
2025-04-16 19:31:35 -0400

    formatted headmates list
    made it to where table will add as many headmates there are with their information
    
    Co-Authored-By: Jaenatheseastar <184808812+Jaenatheseastar@users.noreply.github.com>

--- YUFrontend/src/routes/headmateslist/+page.svelte
+++ YUFrontend/src/routes/headmateslist/+page.svelte
@@ -2,13 +2,15 @@
     /** @type {{ data: import('./$types').PageData }} */
     let { data } = $props();
     import {
+        Button,
         Table,
 		TableHead,
 		TableHeadCell,
 		TableBody,
 		TableBodyRow,
 		TableBodyCell,
-		Checkbox } from 'svelte-5-ui-lib';
+		Checkbox, 
+		Avatar} from 'svelte-5-ui-lib';
 	const headItems = ['Brand', 'Name', 'Color', 'Type', 'Price'];
 	const bodyItems = [
 		['Apple', 'MacBook Pro 17"', 'Sliver', 'Laptop', '$2999'],
@@ -17,66 +19,54 @@
 		['Google', ' Pixel Phone', 'Gray', 'Phone', '$799'],
 		['Apple', 'Watch 5', 'Red', 'Wearables', '$999']
 	];
-
+let {Headmates} = $state([]);
+let {headmateslist} =$state([]);
 </script>
+
+<Button class="bbg- [#77B602] m-4"> Add New Member</Button>
 <Table hoverable={true} class= "m-4 h-100% ">
 	<TableHead>
 		<TableHeadCell class="!p-4">
-			<Checkbox />
+			<Checkbox  />
 		</TableHeadCell>
-		<TableHeadCell>Product name</TableHeadCell>
-		<TableHeadCell>Color</TableHeadCell>
-		<TableHeadCell>Category</TableHeadCell>
-		<TableHeadCell>Price</TableHeadCell>
+		<TableHeadCell>Name</TableHeadCell>
+		<TableHeadCell>Handle</TableHeadCell>
+		<TableHeadCell>Subsystems</TableHeadCell>
+		
 		<TableHeadCell>
 			<span class="sr-only">Edit</span>
 		</TableHeadCell>
-	</TableHead>
-	<TableBody class="divide-y">
-		<TableBodyRow>
-			<TableBodyCell class="!p-4">
-				<Checkbox />
-			</TableBodyCell>
-			<TableBodyCell>Apple MacBook Pro 17"</TableBodyCell>
-			<TableBodyCell>Sliver</TableBodyCell>
-			<TableBodyCell>Laptop</TableBodyCell>
-			<TableBodyCell>$2999</TableBodyCell>
-			<TableBodyCell>
-				<a
-					href="/components/table"
-					class="text-primary-600 dark:text-primary-500 font-medium hover:underline">Edit</a
-				>
-			</TableBodyCell>
-		</TableBodyRow>
-		<TableBodyRow>
-			<TableBodyCell class="!p-4">
-				<Checkbox />
-			</TableBodyCell>
-			<TableBodyCell>Microsoft Surface Pro</TableBodyCell>
-			<TableBodyCell>White</TableBodyCell>
-			<TableBodyCell>Laptop PC</TableBodyCell>
-			<TableBodyCell>$1999</TableBodyCell>
-			<TableBodyCell>
-				<a
-					href="/components/table"
-					class="text-primary-600 dark:text-primary-500 font-medium hover:underline">Edit</a
-				>
-			</TableBodyCell>
-		</TableBodyRow>
-		<TableBodyRow>
-			<TableBodyCell class="!p-4">
-				<Checkbox />
-			</TableBodyCell>
-			<TableBodyCell>Magic Mouse 2</TableBodyCell>
-			<TableBodyCell>Black</TableBodyCell>
-			<TableBodyCell>Accessories</TableBodyCell>
-			<TableBodyCell>$99</TableBodyCell>
-			<TableBodyCell>
-				<a
-					href="/components/table"
-					class="text-primary-600 dark:text-primary-500 font-medium hover:underline">Edit</a
-				>
-			</TableBodyCell>
-		</TableBodyRow>
-	</TableBody>
+	</TableHead><TableBody>
+    {#await data}
+    <TableBodyRow>no members listed
+        <TableBodyCell>Apple MacBook Pro 17"</TableBodyCell>
+       </TableBodyRow>
+    {:then} 
+    {#each data.dummydata.Headmates as member}
+<TableBodyRow>
+    <TableBodyCell class="!p-4">
+        <Checkbox />
+    </TableBodyCell>
+    <TableBodyCell>{member.name}</TableBodyCell>
+    <TableBodyCell>{member.handle}</TableBodyCell>
+    <TableBodyCell>{member.subsystems}</TableBodyCell>
+    
+    <TableBodyCell>
+        <a
+            href="/components/table"
+            class="text-primary-600 dark:text-primary-500 font-medium hover:underline">Edit</a
+        >
+    </TableBodyCell>
+    
+</TableBodyRow>
+{/each}
+      
+    {/await}
+   
+   
+   
+  
+
+</TableBody>
+
 </Table>
