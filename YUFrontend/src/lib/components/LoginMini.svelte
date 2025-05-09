63204838519ae86235fe21ea11504f26e6531362
AmaratheCapybara <AmaranthAragon@gmail.com>
2025-05-09 02:13:18 -0400

    Update LoginMini.svelte
    changed mini button and text color to match the color theme

--- YUFrontend/src/lib/components/LoginMini.svelte
+++ YUFrontend/src/lib/components/LoginMini.svelte
@@ -27,11 +27,11 @@
 	<p class="font-semibold mb-2">Login</p>
 	<input placeholder="Username" bind:value={username} class="w-full mb-2 p-1 border rounded" />
 	<input placeholder="Password" type="password" bind:value={password} class="w-full mb-2 p-1 border rounded" />
-	<button on:click={handleLogin} class="bg-blue-500 text-white px-3 py-1 rounded w-full mb-2">
+	<button on:click={handleLogin} class="bg-[#77B602] text-white px-3 py-1 rounded w-full mb-2">
 		Log In
 	</button>
 
-	<button on:click={goToRegister} class="text-sm text-blue-600 hover:underline w-full">
+	<button on:click={goToRegister} class="text-sm text-[#77B602] hover:underline w-full">
 		Create an account
 	</button>
 
