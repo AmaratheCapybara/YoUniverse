eb6f45596bfcec66194b1606ab758d58fd44a43c
AmaratheCapybara <goofydogismyspiritanimal@gmail.com>
2025-06-19 23:03:09 -0400

    donation
    added patreon link to donation page

--- YUFrontend/src/routes/donation/+page.svelte
+++ YUFrontend/src/routes/donation/+page.svelte
@@ -8,7 +8,7 @@
 <iframe id='kofiframe' src='https://ko-fi.com/youniverseproject/?hidefeed=true&widget=true&embed=true&preview=true' style='border:none;width:100%;padding:4px;background:#f9f9f9;' height='712' title='youniverseproject'></iframe>
 <Card>
     <section><h2>Or become a member on our patreon</h2></section>
-    <Button href =''>
+    <Button href ='patreon.com/YoUniverseProject'>
         <image src= '$images/fhd_Patreon_Symbol_6fff9723d3.png'/>
         <image src= '$images/fhd_Patreon_Wordmark_fb38c295a1.png'/>
     </Button>
