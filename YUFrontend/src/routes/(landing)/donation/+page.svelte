b9cf19a40c418470aed8d5e425d919ffa9038bac
Aurorathebadass <auroratheamazon@gmail.com>
2025-06-20 21:00:50 -0400

    update donation
    added card to kofi and changed title above patreon to h1. I would like to change the button to a normal one if we can't get the flowbite ones to work. I checked the app.css and I already formatted the normal buttons so it should be good to go

--- YUFrontend/src/routes/(landing)/donation/+page.svelte
+++ YUFrontend/src/routes/(landing)/donation/+page.svelte
@@ -5,9 +5,12 @@
     /** @type {{ data: import('./$types').PageData }} */
     let { data } = $props();
 </script>
-<iframe id='kofiframe' src='https://ko-fi.com/youniverseproject/?hidefeed=true&widget=true&embed=true&preview=true' style='border:none;width:100%;padding:4px;background:#f9f9f9;' height='712' title='youniverseproject'></iframe>
 <Card>
-    <section><h2>Or become a member on our patreon</h2></section>
+    <h1>One time donation or custom </h1>
+<iframe id='kofiframe' src='https://ko-fi.com/youniverseproject/?hidefeed=true&widget=true&embed=true&preview=true' style='border:none;width:100%;padding:4px;background:#f9f9f9;' height='712' title='youniverseproject'></iframe>
+</Card>
+<Card>
+    <section><h1>Or become a member on our patreon</h1></section>
     <Button class='h-md w-md' href ='patreon.com/YoUniverseProject'>
         <image src= '$images/fhd_Patreon_Symbol_6fff9723d3.png'/>
         <image src= '$images/fhd_Patreon_Wordmark_fb38c295a1.png'/>
