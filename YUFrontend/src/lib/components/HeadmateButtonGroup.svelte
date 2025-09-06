d48109e77925d88c20b2b63f555c43b2bd5deaea
AmaratheCapybara <AmaranthAragon@gmail.com>
2025-09-06 01:46:30 -0400

    Create HeadmateButtonGroup.svelte
    Co-Authored-By: Aurorathebadass <184870254+Aurorathebadass@users.noreply.github.com>

---
+++ YUFrontend/src/lib/components/HeadmateButtonGroup.svelte
@@ -0,0 +1,13 @@
+<ButtonGroup>
+				{#each Profiles as sender}
+					<!-- Button with dynamically applied background color -->
+					<button
+						class="sender-button {sender === senderInput ? 'active' : ''}"
+						on:click={() => setSender(sender)}
+						style="background-color: {sender.FavoriteColor}"
+					>
+						<span>{sender.Name}</span>
+						<div><Avatar size="md" src={sender.ProfilePic} alt={sender.Name} rounded /></div>
+					</button>
+				{/each}
+			</ButtonGroup>
