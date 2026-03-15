60d0d0e73e0f50b4fc563f8b1522e32fa290c308
AmaratheCapybara <AmaranthAragon@gmail.com>
2026-03-14 22:12:33 -0400

    added main page and layout
    Co-Authored-By: Jaenatheseastar <184808812+Jaenatheseastar@users.noreply.github.com>

---
+++ YUFrontend/src/routes/(landing)/+layout.svelte
@@ -0,0 +1,17 @@
+<script>
+	import '../app.css';
+import Footer from '../lib/components/Footer.svelte';
+import Navbar from '$lib/components/Navbar.svelte';
+	import BottomButtons from '$lib/components/BottomButtons.svelte';
+	let { children } = $props();
+</script>
+<Navbar></Navbar>
+<div class="my-20 lg:my-30">
+{@render children()}
+</div>
+
+<Footer></Footer>
+
+
+
+
---
+++ YUFrontend/src/routes/(landing)/+page.js
@@ -0,0 +1,11 @@
+/** @type {import('./$types').PageLoad} */
+export async function load() {
+    // On page load or when changing themes, best to add inline in `head` to avoid FOUC
+/*document.documentElement.classList.toggle(
+  "dark",
+  localStorage.theme === "dark" ||
+    (!("theme" in localStorage) && window.matchMedia("(prefers-color-scheme: dark)").matches),
+);
+*/
+    return {};
+};
