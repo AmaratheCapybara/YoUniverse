376e20536f6400813a48245b18751792f11640f3
AmaratheCapybara <AmaranthAragon@gmail.com>
2026-04-07 19:36:26 -0400

    idk homie
    Co-Authored-By: codenameEDawg <185463837+codenameEDawg@users.noreply.github.com>

--- YUFrontend/src/lib/stores/functions.svelte.js
+++ YUFrontend/src/lib/stores/functions.svelte.js
@@ -11,23 +11,53 @@ export function changeProfile(NewProfileID) {
 		console.log("The profile you are trying to set the selected profile is not computing. Specifically it is registering as undefined");
 	}
 	else if (NewProfileID === null) {
-		console.log('Homie G, I have no idea what you did but the profile you are trying to set the selected profile is registering as null. Something in the coding and setting the id you want to look up ');
+		console.log('Homie G, I have no idea what you did but the profile you are trying to set the selected profile is registering as null. ' +
+			'Something in the coding and setting the id you want to look up ');
 	}
 		else {
 			console.log("Something is very busted. Look at the change profile function I guess.")
 		//hi, welcome back. Something broke pretty bad, huh? -E-Dawg
 	}
-};
+}
 
 export function Add2Front(SelectedProfileID) {
 	if (SelectedProfileID !== undefined || SelectedProfileID !== null) {
 		let FoundProfile = Account.Profiles.find((profile)=>profile.id === SelectedProfileID);
 		Account.Fronters.push(FoundProfile);
-	}
-	else if (SelectedProfileID === undefined) {
 
+		return Account.Fronters;
+	}
+
+
+
+	//debugging instructions for Aurora and others who may need it (no judgments)
+
+	else if (SelectedProfileID === undefined) {
+console.log("The ProfileID is not getting into the function.")
 	}
 	else if (SelectedProfileID === null) {
+console.log("Adding to the Front is very fucked up. The Selected Profile ID that goes into the function Add2Front has been nullified.")
+	}
+}
+
+export function RemoveFromFront(SelectedProfileID) {
+	if (SelectedProfileID !== undefined || SelectedProfileID !== null) {
+		let FoundProfile = Account.Profiles.find((profile)=>profile.id === SelectedProfileID);
+		//remove from Fronters array in account (Account.Fronters)
+
+		return Account.Fronters;
+	}
+}
+export function SelectSender(NewSenderID) {
+	if (NewSenderID !== undefined || NewSenderID !== null) {
+
+
+		return Sender;
+	}
+	else if (NewSenderID === undefined) {
+		console.log("The NewSenderID is not getting into the function.")
+	}
+	else if (NewSenderID === null) {
 
 	}
 }
--- YUFrontend/src/routes/(landing)/FAQ/+page.svelte
+++ YUFrontend/src/routes/(landing)/FAQ/+page.svelte
@@ -10,6 +10,7 @@
         <h1>Will there be an offline version?</h1>
         <h1>How will YoUniverse protect our privacy?</h1>
         <h1>Can I volunteer?</h1>
+        <h1> What's taking so long? </h1> // A lot has to be custom coded, We are taking the time to work with and collaborate with the community
       </section>
     </article>
 -->
