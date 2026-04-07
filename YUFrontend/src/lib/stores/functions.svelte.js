e3ffa0abd48bc41a01a9f7ba9b5e2665209dc996
AmaratheCapybara <AmaranthAragon@gmail.com>
2026-04-07 15:51:08 -0400

    Update functions.svelte.js
    made change profile function and add to front profile
    
    Co-Authored-By: codenameEDawg <185463837+codenameEDawg@users.noreply.github.com>

--- YUFrontend/src/lib/stores/functions.svelte.js
+++ YUFrontend/src/lib/stores/functions.svelte.js
@@ -1,6 +1,33 @@
 import { Account } from '$lib/stores/Account.svelte.js';
+let {SelectedProfile=$bindable()} =$props()
 
 
 export function changeProfile(NewProfileID) {
-	SelectedProfile = Account.SelectedProfile
+	if (NewProfileID !== undefined || NewProfileID !== null) {
+	SelectedProfile = Account.Profiles.find((profile)=>profile.id === NewProfileID);
+	//then update to database
+	}
+	else if (NewProfileID === undefined) {
+		console.log("The profile you are trying to set the selected profile is not computing. Specifically it is registering as undefined");
+	}
+	else if (NewProfileID === null) {
+		console.log('Homie G, I have no idea what you did but the profile you are trying to set the selected profile is registering as null. Something in the coding and setting the id you want to look up ');
+	}
+		else {
+			console.log("Something is very busted. Look at the change profile function I guess.")
+		//hi, welcome back. Something broke pretty bad, huh? -E-Dawg
+	}
+};
+
+export function Add2Front(SelectedProfileID) {
+	if (SelectedProfileID !== undefined || SelectedProfileID !== null) {
+		let FoundProfile = Account.Profiles.find((profile)=>profile.id === SelectedProfileID);
+		Account.Fronters.push(FoundProfile);
+	}
+	else if (SelectedProfileID === undefined) {
+
+	}
+	else if (SelectedProfileID === null) {
+
+	}
 }
