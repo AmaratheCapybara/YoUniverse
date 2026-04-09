e465f46634813cf6bfeab2ae98d9cbad42330406
AmaratheCapybara <AmaranthAragon@gmail.com>
2026-04-09 18:10:17 -0400

    Create styling.svelte.js
    created to make stylings java variables that can be used within the code. added colors including light and dark mode variations
    
    Co-Authored-By: Aurorathebadass <184870254+Aurorathebadass@users.noreply.github.com>

---
+++ YUFrontend/src/lib/stores/styling.svelte.js
@@ -0,0 +1,12 @@
+export const MainThemeColors = {
+	AccentColor: '#77B602',
+	AlertColor: '#FF6F61',
+	ThirdLively: '#F4A261',
+	ThirdCalm: '#B4D2FF'
+};
+export const ChangingColors = {
+	LightmodeBackground: '#FFFFFF',
+	DarkmodeBackground: '#050C1D',
+	LightmodeText: '#050C1D',
+	DarkmodeText: '#FFFFFF'
+};
