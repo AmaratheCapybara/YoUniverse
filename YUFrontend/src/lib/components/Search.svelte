8501bc678ccf4312b9b874766f5bd5875378d98b
AmaratheCapybara <AmaranthAragon@gmail.com>
2026-04-09 18:11:13 -0400

    create search component
    just readjusted the flowbite search in a way that made sense to me. check proporties to see what can be added
    
    Co-Authored-By: Aurorathebadass <184870254+Aurorathebadass@users.noreply.github.com>

---
+++ YUFrontend/src/lib/components/Search.svelte
@@ -0,0 +1,19 @@
+<script>
+    import {Search, Button} from "flowbite-svelte";
+    import {MainThemeColors} from "$lib/stores/styling.svelte.js";
+    import {Searching} from "$lib/stores/functions.svelte.js";
+    let {
+        SearchInput,
+        Placeholder,
+        Filter,
+        ListArray,
+        AvailableTags
+    } = $props();
+</script>
+<form>
+    <Search
+            value={SearchInput}
+            placeholder={Placeholder}
+    ></Search>
+    <Button color={MainThemeColors.AccentColor} pill outiline="true" shadow="false" onclick={Searching(SearchInput , ListArray)} type="submit">Find</Button>
+</form>
