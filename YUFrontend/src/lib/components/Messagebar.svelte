71bf431c308a8993ce7c8e156db4cb41be626386
AmaratheCapybara <AmaranthAragon@gmail.com>
2026-04-09 19:37:21 -0400

    Update Messagebar.svelte
    updated imports and props
    
    Co-Authored-By: Aurorathebadass <184870254+Aurorathebadass@users.noreply.github.com>

--- YUFrontend/src/lib/components/Messagebar.svelte
+++ YUFrontend/src/lib/components/Messagebar.svelte
@@ -1,3 +1,16 @@
-<button on:click={sendMessage} disabled={!senderInput} style="background-color: {sender.favoriteColor}">
-    <img src={sender.ProfilePic} alt={sender} > Send
+<script>
+    import {MainThemeColors} from "$lib/stores/styling.svelte.js";
+    import {blanklogotransparent} from "$lib/images/blanklogotransparent.png";
+    import {SendMessage} from "$lib/stores/functions.svelte.js";
+
+    let {
+        Sender = {
+            Name:"Sender",
+            FavoriteColor: MainThemeColors.AccentColor,
+            ProfilePic: blanklogotransparent
+        }
+    } = $props();
+</script>
+<button onclick={SendMessage()} disabled={!senderInput} style="background-color: {Sender.FavoriteColor}">
+    <img src={Sender.ProfilePic} alt={sender} > Send
    </button>
