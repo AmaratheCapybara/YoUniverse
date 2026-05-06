5edd397bd3c3d720764f6c469ebf776cd63ac48e
AmaratheCapybara <AmaranthAragon@gmail.com>
2026-05-06 19:37:12 -0400

    Update HeadmateButtonGroup.svelte
    made picture snippets
    
    Co-Authored-By: Aurorathebadass <184870254+Aurorathebadass@users.noreply.github.com>

--- YUFrontend/src/lib/components/HeadmateButtonGroup.svelte
+++ YUFrontend/src/lib/components/HeadmateButtonGroup.svelte
@@ -3,8 +3,19 @@
 		 ButtonGroup,
 			 Avatar
 	 } from "flowbite-svelte";
+	 import BluePlanet from '$lib/images/Assets/blue transparent.png';
+	 import HappyMoon from '$lib/images/Assets/happy moon.png';
+	 import Ice from '$lib/images/Assets/ice transparent.png';
+	 import Life from '$lib/images/Assets/life transparency.png';
+	 import Terrestrial from '$lib/images/Assets/terrestrial transparency.png';
 	 let {Profiles} =$props();
  </script>
+ {#snippet HappyMoon()}
+
+	 {/snippet}
+ {#snippet blue()}
+	 {BluePlanet}
+	 {/snippet}
 <ButtonGroup>
 				{#each Profiles as sender}
 					<!-- Button with dynamically applied background color -->
