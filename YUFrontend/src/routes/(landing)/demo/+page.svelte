2690ffd1930596a63c3a3b2492d423016c14ca30
AmaratheCapybara <AmaranthAragon@gmail.com>
2026-05-08 05:04:15 -0400

    Update +page.svelte
    made a todo and things needed to come through in order to decide what will be shown on the page when
    
    Co-Authored-By: codenameEDawg <185463837+codenameEDawg@users.noreply.github.com>

--- YUFrontend/src/routes/(landing)/demo/+page.svelte
+++ YUFrontend/src/routes/(landing)/demo/+page.svelte
@@ -1,4 +1,8 @@
 <script>
     import ChatWindow from "$lib/components/blocks/ChatWindow.svelte";
+    import {Account} from "$lib/stores/Account.svelte.js";
+
+    let SelectedProfile = $derived(Account.SelectedProfile);
 </script>
+<!--todo add conversation selector-->
 <ChatWindow></ChatWindow>
